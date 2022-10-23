67aa048125769074b981bfaf10f99ea441b47e0e
unknown <selmarionx@gmail.com>
2022-10-23 10:27:48 +0300

    task1Done

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,3 +2,14 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
+
+Console.Clear();
+
+void Main()
+{
+    Console.Write("Введите числа ЧЕРЕЗ ПРОБЕЛ: ");
+    int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    int count = arr.Count(x => x > 0);
+    Console.WriteLine($"Кол-во элементов больше нуля: {count}");
+} 
+Main();
